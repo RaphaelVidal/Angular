a8fd928da4f22d94c5f87de7736549f32489997a
Raphael Vidal <raphael.r.vidal@gmail.com>
2019-09-24 20:30:15 -0300

    compra carro

--- typescript/dist/orientacao_a_objetos.js
+++ typescript/dist/orientacao_a_objetos.js
@@ -6,20 +6,39 @@ var Carro = /** @class */ (function () {
         this.numeroDePortas = pnumeroDePortas;
         this.velocidade = pvelocidade;
     }
-    Carro.prototype.acelerar = function () {
-        this.velocidade = this.velocidade + 10;
-    };
-    Carro.prototype.parar = function () {
-        this.velocidade = 0;
-    };
-    Carro.prototype.velocidadeAtual = function () {
-        return this.velocidade;
-    };
+    Carro.prototype.acelerar = function () { this.velocidade = this.velocidade + 10; };
+    Carro.prototype.parar = function () { this.velocidade = 0; };
+    Carro.prototype.velocidadeAtual = function () { return this.velocidade; };
     return Carro;
 }());
-var carroA = new Carro('Sandero', 5, 20);
-carroA.acelerar();
-// console.log(carroA);
-// carroA.parar()
-console.log(carroA);
-console.log(carroA.velocidadeAtual());
+var Concessionaria = /** @class */ (function () {
+    function Concessionaria(pendereco, plistadeCarros) {
+        this.endereco = pendereco;
+        this.listadeCarros = plistadeCarros;
+    }
+    Concessionaria.prototype.fornecedor = function () { return this.endereco; };
+    Concessionaria.prototype.mostrarListaDeCarros = function () { return this.listadeCarros; };
+    return Concessionaria;
+}());
+var Pessoa = /** @class */ (function () {
+    function Pessoa(pnome, pcarroPreferido) {
+        this.nome = pnome;
+        this.carroPreferido = pcarroPreferido;
+    }
+    Pessoa.prototype.dizerNome = function () { return this.nome; };
+    Pessoa.prototype.dizerCarroPreferido = function () { return this.carroPreferido; };
+    Pessoa.prototype.comprarCarro = function (pcarro) { this.carro = pcarro; };
+    Pessoa.prototype.dizerCarroQueTem = function () { return this.carro; };
+    return Pessoa;
+}()); // fim pessoa
+// criar carros para o vetor
+var carroA = new Carro('Sandero', 5, 10);
+var carroB = new Carro('Hilux', 6, 20);
+var carroC = new Carro('Fusca', 8, 32);
+// criar vetor de carros
+var listaDeCarros = [carroA, carroB, carroC]; // ou let listaDeCarros:Carro[] = [carroA,carroB,carroC];
+var concessionariaA = new Concessionaria('AV. Brasil', listaDeCarros);
+// console.log(concessionariaA.mostrarListaDeCarros()); 
+var pessoaA = new Pessoa('Emanuella Vidal', 'Sorento');
+pessoaA.comprarCarro(carroA);
+console.log(pessoaA.dizerCarroQueTem());
--- typescript/orientacao_a_objetos.ts
+++ typescript/orientacao_a_objetos.ts
@@ -1,31 +1,42 @@
-class Carro {
-    private modelo: string
-    private numeroDePortas: number
-    private velocidade: number = 0
-
+class Carro {  private modelo: string; private numeroDePortas: number; private velocidade: number = 0; 
     constructor(pmodelo: string, pnumeroDePortas: number, pvelocidade: number) {
         this.modelo = pmodelo
         this.numeroDePortas = pnumeroDePortas
         this.velocidade = pvelocidade
     }
-
-    public acelerar(): void {
-        this.velocidade = this.velocidade + 10
-
-    }
-    public parar(): void {
-        this.velocidade = 0
-    }
-    public velocidadeAtual(): number {
-        return this.velocidade
-    }
-
+    public acelerar(): void { this.velocidade = this.velocidade + 10 }
+    public parar(): void { this.velocidade = 0 }
+    public velocidadeAtual(): number { return this.velocidade }
 }
 
-let carroA = new Carro('Sandero',5,20)
-carroA.acelerar()
-// console.log(carroA);
-// carroA.parar()
-console.log(carroA);
+class Concessionaria { private endereco: string;  private listadeCarros: Array<Carro>;
+    constructor(pendereco: string,plistadeCarros:Array<Carro>) { this.endereco = pendereco; this.listadeCarros = plistadeCarros }
+    public fornecedor(): string { return this.endereco }
+    public mostrarListaDeCarros(): Array<Carro> { return this.listadeCarros }
+}
 
-console.log(carroA.velocidadeAtual());
+class Pessoa { private nome: string; private carroPreferido: string;   private carro: any;
+    constructor(pnome: string, pcarroPreferido: string) { this.nome = pnome; this.carroPreferido = pcarroPreferido }
+    public dizerNome(): string { return this.nome }
+    public dizerCarroPreferido(): string { return this.carroPreferido }
+    public comprarCarro(pcarro: Carro): void { this.carro = pcarro }
+    public dizerCarroQueTem(): string { return this.carro }
+}// fim pessoa
+
+// criar carros para o vetor
+let carroA = new Carro('Sandero', 5, 10); let carroB = new Carro('Hilux', 6, 20); let carroC = new Carro('Fusca', 8, 32)
+
+// criar vetor de carros
+let listaDeCarros: Array<Carro> = [carroA,carroB,carroC]; // ou let listaDeCarros:Carro[] = [carroA,carroB,carroC];
+
+let concessionariaA = new Concessionaria('AV. Brasil', listaDeCarros);
+
+// console.log(concessionariaA.mostrarListaDeCarros()); 
+
+let pessoaA = new Pessoa('Emanuella Vidal','Sorento')
+
+pessoaA.comprarCarro(carroA);
+
+console.log(pessoaA.dizerCarroQueTem());
+
+
